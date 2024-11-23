f37c1ff3e47d7572777b7f66ef59e352d59f433a
bpengu1n <echo@pwnies.dev>
2024-11-22 19:24:01 -0600

    updated

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ from sqlalchemy.orm import Session
 
 engine = create_engine("sqlite:///test.db", echo=True)
 
-from model import Story
+from model import Story as schema
 
 
-Story.Base.metadata.create_all(engine)
+schema.Base.metadata.create_all(engine)
 
-test_story = Story.Story()
+cat_anime = schema.Category(id=5, label="Anime & Manga")
+cat_movies = schema.Category(id=2, label="Movies")
 
+author_test = schema.Author(id=23, name="Jane Doe")
+fandom_fma = schema.Fandom(id=1, label="Full Metal Alchemist", category=cat_anime)
+fandom_hp = schema.Fandom(id=2, label="Harry Potter", category=cat_movies)
+
+test_stories = (
+    schema.Story(id=52, title="test", 
+                          fandoms=[fandom_fma], author=author_test),
+    schema.Story(id=59, title="test2", 
+                          fandoms=[fandom_fma, fandom_hp], author=author_test)
+)
 
 with Session(engine) as session:
-    session.add(test_story)
+    session.add_all(test_stories)
     session.commit()
--- model/Schema.py
+++ model/Schema.py
@@ -1,7 +1,9 @@
 from typing import List
 from typing import Optional
+from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
+from sqlalchemy import Table
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -10,22 +12,40 @@ from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     pass
 
+fandom_story_assoc = Table(
+    "fandom_story_assoc",
+    Base.metadata,
+    Column("fandom_id", ForeignKey("fandom.id")),
+    Column("story_id", ForeignKey("story.id")),
+)
+
 class Category(Base):
     __tablename__ = "category"
     id: Mapped[int] = mapped_column(primary_key=True)
-    label: Mapped[str] = mapped_column(String(6))
-    stories: Mapped[List["Story"]] = relationship(
+    label: Mapped[str] = mapped_column(String(255))
+    fandoms: Mapped[List["Fandom"]] = relationship(
         back_populates="category", cascade="all, delete-orphan"
     )
     def __repr__(self) -> str:
-        return f"Category(id={self.id!r}, name={self.label!r})"
+        return f"Category(id={self.id!r}, label={self.label!r})"
 
 
+class Fandom(Base):
+    __tablename__ = "fandom"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    label: Mapped[str] = mapped_column(String(255))
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
+    category: Mapped["Category"] = relationship(back_populates="fandoms")
+    stories: Mapped[Optional[List["Story"]]] = relationship(
+        secondary=fandom_story_assoc, back_populates="fandoms"
+    )
+    def __repr__(self) -> str:
+        return f"Fandom(id={self.id!r}, label={self.label!r})"
 
 class Author(Base):
     __tablename__ = "author"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(6))
+    name: Mapped[str] = mapped_column(String(255))
     stories: Mapped[List["Story"]] = relationship(
         back_populates="author", cascade="all, delete-orphan"
     )
@@ -36,7 +56,8 @@ class Author(Base):
 class Story(Base):
     __tablename__ = "story"
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(128))
+    title: Mapped[str] = mapped_column(String(255))
+    fandoms: Mapped[List["Fandom"]] = relationship(secondary=fandom_story_assoc, back_populates="stories")
     author_id: Mapped[int] = mapped_column(ForeignKey("author.id"))
     author: Mapped["Author"] = relationship(back_populates="stories")
     def __repr__(self) -> str:
